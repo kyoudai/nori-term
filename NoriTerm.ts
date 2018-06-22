44419c844e42b78132bbadbc1712c102daf21c4f
Andrei Nemes <andrei.nemes@fishinabottle.com>
2018-06-22 17:24:40 +0100

    remove private underscore notation; remove 'magic' numbers

--- NoriTerm.ts
+++ NoriTerm.ts
@@ -1,50 +1,50 @@
 class NoriTerm {
   public getCanvas(): HTMLCanvasElement {
-    return this.canvas_;
+    return this.canvas;
   }
 
   public clear(): void {
-    this.context_.fillStyle = this.colorBackground_;
-    this.context_.fillRect(0, 0, this.canvas_.width, this.canvas_.height);
+    this.context.fillStyle = this.colorBackground;
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
   public clearLine(): void {
-    this.clearRegion(0, this.cursorPosition_[1], this.columns_, 1);
-    this.moveCursor(0, this.cursorPosition_[1]);
+    this.clearRegion(0, this.cursorPosition[1], this.columns, 1);
+    this.moveCursor(0, this.cursorPosition[1]);
   }
 
   public put(char: string): void {
     const [charw, charh] = this.getBlockSize();
-    const [x, y] = this.cursorPosition_;
+    const [x, y] = this.cursorPosition;
 
-    if (x + 1 >= this.columns_) {
-      this.moveCursor(0, this.cursorPosition_[1] + 1);
+    if (x + 1 >= this.columns) {
+      this.moveCursor(0, this.cursorPosition[1] + 1);
     } else {
-      this.moveCursor(this.cursorPosition_[0] + 1, this.cursorPosition_[1]);
+      this.moveCursor(this.cursorPosition[0] + 1, this.cursorPosition[1]);
     }
 
-    this.context_.font = this.getFont();
-    this.context_.textAlign = "center";
-    this.context_.textBaseline = "middle";
-    this.context_.fillStyle = this.currentColor_;
-    this.context_.fillText(char, x * charw + charw / 2, y * charh + charh / 2);
+    this.context.font = this.getFont();
+    this.context.textAlign = "center";
+    this.context.textBaseline = "middle";
+    this.context.fillStyle = this.currentColor;
+    this.context.fillText(char, x * charw + charw / 2, y * charh + charh / 2);
   }
 
   public remove(): void {
-    this.moveCursor(this.cursorPosition_[0] - 1, this.cursorPosition_[1]);
+    this.moveCursor(this.cursorPosition[0] - 1, this.cursorPosition[1]);
   }
 
   public newLine(): void {
-    this.moveCursor(0, this.cursorPosition_[1] + 1);
+    this.moveCursor(0, this.cursorPosition[1] + 1);
   }
 
   public resetColor(): void {
-    this.currentColor_ = this.colorForeground_;
+    this.currentColor = this.colorForeground;
     this.fontIsBold = false;
   }
 
   public setColor(index: number): void {
-    this.currentColor_ = this.colorPalette_[index - 1];
+    this.currentColor = this.colorPalette[index - 1];
   }
 
   public setBold(): void {
@@ -52,33 +52,33 @@ class NoriTerm {
   }
 
   public getBackgroundColor(): string {
-    return this.colorBackground_;
+    return this.colorBackground;
   }
 
   public moveCursor(x: number, y: number): void {
     const [charw, charh] = this.getBlockSize();
 
-    this.clearRegion.apply(this, this.cursorPosition_);
+    this.clearRegion.apply(this, this.cursorPosition);
 
-    this.cursorPosition_ = [x, y];
+    this.cursorPosition = [x, y];
 
-    this.context_.fillStyle = this.colorCursor_;
-    this.context_.fillRect(
-      this.cursorPosition_[0] * charw - 0.5,
-      this.cursorPosition_[1] * charh - 0.5,
+    this.context.fillStyle = this.colorCursor;
+    this.context.fillRect(
+      this.cursorPosition[0] * charw,
+      this.cursorPosition[1] * charh,
       charw,
       charh
     );
-    this.context_.fill();
+    this.context.fill();
   }
 
   constructor() {
-    this.context_.font = this.getFont();
+    this.context.font = this.getFont();
 
     const [charw, charh] = this.getBlockSize();
 
-    this.canvas_.width = this.columns_ * charw;
-    this.canvas_.height = this.rows_ * charh;
+    this.canvas.width = this.columns * charw;
+    this.canvas.height = this.rows * charh;
 
     this.clear();
     this.moveCursor(0, 0);
@@ -87,40 +87,41 @@ class NoriTerm {
   private clearRegion(x: number, y: number, width: number = 1, height: number = 1): void {
     const [charw, charh] = this.getBlockSize();
 
-    this.context_.fillStyle = this.colorBackground_;
-    this.context_.fillRect(
-      x * charw - 1,
-      y * charh - 1,
-      width * charw + 2,
-      height * charh + 2
+    this.context.fillStyle = this.colorBackground;
+    this.context.fillRect(
+      x * charw,
+      y * charh,
+      width * charw,
+      height * charh
     );
-    this.context_.fill();
+    this.context.fill();
   }
 
   private getBlockSize(): [number, number] {
-    this.context_.font = this.getFont();
-    return [this.context_.measureText('A').width, this.fontSize_ + Math.floor(this.fontSize_ * 0.45)];
+    this.context.font = this.getFont();
+    return [Math.ceil(this.context.measureText('W').width), Math.ceil(this.fontSize * this.fontLineHeight)];
   }
 
   private getFont(): string {
-    return `${this.fontIsBold ? 'bold ' : ''}${this.fontSize_}pt ${this.fontFamily_}`
+    return `${this.fontIsBold ? 'bold ' : ''}${this.fontSize}pt ${this.fontFamily}`
   }
 
-  private canvas_: HTMLCanvasElement = document.createElement('canvas');
-  private context_: CanvasRenderingContext2D = this.canvas_.getContext('2d')!;
+  private canvas: HTMLCanvasElement = document.createElement('canvas');
+  private context: CanvasRenderingContext2D = this.canvas.getContext('2d')!;
 
-  private columns_: number = 90;
-  private rows_: number = 32;
-  private cursorPosition_: [number, number] = [0, 0];
+  private columns: number = 90;
+  private rows: number = 32;
+  private cursorPosition: [number, number] = [0, 0];
 
-  private colorBackground_: string = '#0d2619';
-  private colorForeground_: string = '#d9e6f2';
-  private colorCursor_: string = '#d9e6f2';
-  private colorPalette_: string[] = ['#000000', '#b87a7a', '#7ab87a', '#b8b87a', '#7a7ab8', '#b87ab8', '#7ab8b8', '#d9d9d9', '#262626', '#dbbdbd', '#bddbbd', '#dbdbbd', '#bdbddb', '#dbbddb', '#bddbdb', '#ffffff']
+  private colorBackground: string = '#0d2619';
+  private colorForeground: string = '#d9e6f2';
+  private colorCursor: string = '#d9e6f2';
+  private colorPalette: string[] = ['#000000', '#b87a7a', '#7ab87a', '#b8b87a', '#7a7ab8', '#b87ab8', '#7ab8b8', '#d9d9d9', '#262626', '#dbbdbd', '#bddbbd', '#dbdbbd', '#bdbddb', '#dbbddb', '#bddbdb', '#ffffff']
 
-  private currentColor_: string = this.colorForeground_;
+  private currentColor: string = this.colorForeground;
 
-  private fontSize_: number = 12;
-  private fontFamily_: string = 'Monaco, Courier, monospace';
+  private fontSize: number = 12;
+  private fontLineHeight: number = 1.45;
+  private fontFamily: string = '"Noto Mono", "DejaVu Sans Mono", Monaco, Courier, monospace';
   private fontIsBold: boolean = false;
 }
